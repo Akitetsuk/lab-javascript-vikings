f0e62cf726d656b348269e19b5b2bdf4aa00421b
Míriam <m.feijoo.1995@gmail.com>
2022-03-27 19:13:37 +0200

    falta 4 i 5

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,76 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
 
-// Viking
-class Viking {}
+  attack(){
+    return this.strength;
+  }
+  
+  receiveDamage(damage){
+    this.health -= damage;
+  }
+}
+
+// Vikingf
+class Viking extends Soldier { // extender clase
+  constructor( name,health, strength) {    
+    super(health,strength); //incializar clase padre
+    this.name = name;   
+  } 
+    
+  receiveDamage(damage){
+      super.receiveDamage(damage);///coger funcion de soldado
+      if(this.health > 0 ){
+        return `${this.name} has received ${damage} points of damage`;
+      }
+      return `${this.name} has died in act of combat`;
+  }
+
+  battleCry(){
+    return "Odin Owns You All!";
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier { // extender clase {
+  constructor( health, strength) {    
+    super(health,strength); //incializar clase padre   
+  } 
 
+  receiveDamage(damage){
+    super.receiveDamage(damage);//coger funcion de soldado
+    if(this.health > 0 ){
+      return `A Saxon has received ${damage} points of damage`;
+    }
+    return `A Saxon has died in combat`;
+  }
+}
 // War
-class War {}
+class War {  
+
+  vikingArmy = new Array();
+  saxonArmy = new Array();
+
+  addViking(){
+    
+  }
+  addSaxon(){
+
+  }
+  vikingAttack(){
+
+  }
+  saxonAttack(){
+
+  }
+  showStatus(){
+    
+  }
+}
 
 
 
